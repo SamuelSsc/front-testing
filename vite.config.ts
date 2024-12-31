ae570c57a47b4099cc692bfae84027fef1c8eee6
SamuelSsc <samuelssc5874@gmail.com>
2024-12-31 15:59:15 -0300

    adjusting comments on pr

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   resolve: {
@@ -9,18 +9,18 @@ export default defineConfig({
       "@app": path.resolve(__dirname, "./src/app"),
       "@components": path.resolve(__dirname, "./src/app/components"),
       "@assets": path.resolve(__dirname, "./src/assets"),
-      "@utils": path.resolve(__dirname, "./src/utils"), 
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
   plugins: [react()],
   build: {
     emptyOutDir: true,
-    outDir: 'dist/public',
+    outDir: "dist/public",
     commonjsOptions: {
-      include: ['tailwind.config.ts', 'node_modules/**'],
+      include: ["tailwind.config.ts", "node_modules/**"],
     },
   },
   optimizeDeps: {
-    include: ['tailwind-config'],
+    include: ["tailwind-config"],
   },
 });
