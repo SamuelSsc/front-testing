b588082e4a420b1eb134313f3eda196a9f6d257c
SamuelSsc <samuelssc5874@gmail.com>
2025-01-05 21:44:07 -0300

    adjusting task's

--- src/app/modules/home/home.page.tsx
+++ src/app/modules/home/home.page.tsx
@@ -41,50 +41,53 @@ function HomePage() {
               {homeStrings.form.button}
             </Button>
           </div>
-          <section className="max-h-[calc(100vh-300px)] overflow-y-auto">
+          <section className="max-h-[calc(100vh-300px)] overflow-y-auto todo-list">
             <AnimatePresence>
               {tasks.length > 0 ? (
                 tasks.map((task) => (
-                  <motion.div
-                    key={task.text}
-                    layout
-                    initial={{ opacity: 0, y: -20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, x: 20 }}
-                    transition={{ duration: 0.3 }}
-                  >
-                    <Card className="py-lg my-sm relative">
-                      <CardBody>
-                        <div className="flex justify-between items-center">
-                          <div className="flex items-center justify-center gap-sm">
-                            {/* toDo: Implementar um atomic para o checkbox */}
-                            <input
-                              type="checkbox"
-                              className="h-md w-md"
-                              checked={task.checked}
-                              onChange={() => toggleTask(task.text)}
-                            />
-                            <BodyPrimary
-                              className={`${
-                                task.checked ? "line-through text-gray-500" : ""
-                              }`}
-                            >
-                              {task.text}
-                            </BodyPrimary>
+                  <li key={task.text} style={{ listStyle: "none" }}>
+                    <motion.div
+                      layout
+                      initial={{ opacity: 0, y: -20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, x: 20 }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <Card className="py-lg my-sm relative">
+                        <CardBody>
+                          <div className="flex justify-between items-center">
+                            <div className="flex items-center justify-center gap-sm">
+                              {/* toDo: Implementar um atomic para o checkbox */}
+                              <input
+                                type="checkbox"
+                                className="h-md w-md"
+                                checked={task.checked}
+                                onChange={() => toggleTask(task.text)}
+                              />
+                              <BodyPrimary
+                                className={`${
+                                  task.checked
+                                    ? "line-through text-gray-500"
+                                    : ""
+                                }`}
+                              >
+                                {task.text}
+                              </BodyPrimary>
+                            </div>
+                            <div className="flex flex-row gap-sm">
+                              <button
+                                className="w-lg h-lg"
+                                onClick={() => removeTask(task.text)}
+                                aria-label={homeStrings.form.ariaLabelDelete}
+                              >
+                                <Trash2 color={Colors.feedback.error} />
+                              </button>
+                            </div>
                           </div>
-                          <div className="flex flex-row gap-sm">
-                            <button
-                              className="w-lg h-lg"
-                              onClick={() => removeTask(task.text)}
-                              aria-label={homeStrings.form.ariaLabelDelete}
-                            >
-                              <Trash2 color={Colors.feedback.error} />
-                            </button>
-                          </div>
-                        </div>
-                      </CardBody>
-                    </Card>
-                  </motion.div>
+                        </CardBody>
+                      </Card>
+                    </motion.div>
+                  </li>
                 ))
               ) : (
                 <BodyPrimary className="text-center mt-xl">
