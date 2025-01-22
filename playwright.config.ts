8dca660b10f8dfc51d8536e4ab650a8dcce8fe13
SamuelSsc <samuelssc5874@gmail.com>
2025-01-21 22:41:50 -0300

    testing suite implements a new cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./src/playwright-tests",
@@ -12,4 +12,30 @@ export default defineConfig({
     video: "retain-on-failure",
     screenshot: "only-on-failure",
   },
+  projects: [
+    {
+      name: "Chromium-Desktop",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "Firefox-Desktop",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "WebKit-Desktop",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "Chromium-Mobile",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Firefox-Mobile",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "WebKit-Mobile",
+      use: { ...devices["Pixel 5"] },
+    },
+  ],
 });
